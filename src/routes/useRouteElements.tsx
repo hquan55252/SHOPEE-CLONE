b1dd8c61fe27240911b5180f3faf5e9533266931
hquan55252 <mailemisoquan@gmail.com>
2025-05-24 12:23:58 +0700

    Code UI trang Login / Register

---
+++ src/routes/useRouteElements.tsx
@@ -0,0 +1,31 @@
+import RegisterLayout from '@/layouts/RegisterLayout'
+import Login from '@/pages/Login'
+import ProductList from '@/pages/ProductList'
+import Register from '@/pages/Register'
+import { useRoutes } from 'react-router-dom'
+
+export default function useRouteElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElements
+}
